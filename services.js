480ebc42cf895789ffc84c3eab65f6d0bd063f87
John Mbusyo <dukembusyo@gmail.com>
2025-02-18 11:58:12 +0000

    huias

--- services.js
+++ services.js
@@ -178,3 +178,14 @@ const services = [
         
 
     
+
+
+
+        function toggleSidebar() {
+            var sidebar = document.getElementById("sidebar");
+            if (sidebar.style.width === "250px") {
+                sidebar.style.width = "0";
+            } else {
+                sidebar.style.width = "250px";
+            }
+        }
