c8310cbe8cffa9dc90e4460a02d1d05f7252b8ca
John Mbusyo <dukembusyo@gmail.com>
2025-02-18 12:42:08 +0000

    hsJYQWS

--- services.js
+++ services.js
@@ -179,14 +179,3 @@ const services = [
 
     
 
-        function toggleSidebar() {
-            var sidebar = document.getElementById("sidebar");
-            var content = document.querySelector(".content");
-            if (sidebar.style.width === "250px") {
-                sidebar.style.width = "0";
-                content.style.marginLeft = "0";
-            } else {
-                sidebar.style.width = "250px";
-                content.style.marginLeft = "250px";
-            }
-        }
